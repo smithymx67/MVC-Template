a194773aa5698b8554fd0777c780c0140231ca90
Sam Smith <sam@samsmith.me>
2019-09-18 09:06:08 +0100

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+printf "==== Running npm install ====\n"
+npm install
+
+printf "\n==== Running composer install on /src ====\n"
+cd src
+composer install
+
+printf "\n==== Running composer install on /tests ====\n"
+cd ../
+cd tests
+composer install
+
+printf "\n==== Running gulp ====\n"
+cd ../
+gulp
