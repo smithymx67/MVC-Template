588f12d180405aaa8deb4f8f2976d88bd9480156
Sam Smith <sam@samsmith.me>
2019-10-27 13:17:58 +0000

    Updated tests and added status codes to error pages

--- src/app/controllers/Errors.php
+++ src/app/controllers/Errors.php
@@ -14,16 +14,19 @@ class Errors extends Controller {
 
   // Route to /errors/404
   public function PageNotFound () {
+    header("HTTP/1.0 404 Not Found");
     $this->view('errors/404');
   }
 
   // Route to /errors/403
   public function ForbiddenAccess () {
+    header("HTTP/1.0 403 Forbidden");
     $this->view('errors/403');
   }
 
   // Route to /errors/500
   public function InternalServerError () {
+    header("HTTP/1.0 500 Internal Server Error");
     $this->view('errors/500');
   }
 }
